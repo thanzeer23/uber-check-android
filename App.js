389562821a722962b1f01b6614e3fb7a581df0b6
thanzeer hussain <thanzeerhussainlrn@gmail.com>
2023-08-08 19:30:45 +0530

    last update

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./screens/mapScreen";
+import EatScreen from "./screens/EatScreen";
+import { KeyboardAvoidingView, Platform } from "react-native";
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                options={{ headerShown: false }}
+                component={HomeScreen}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                options={{
+                  headerShown: false,
+                  gestureDirection: "horizontal",
+                  gestureEnabled: true,
+                }}
+                component={MapScreen}
+              />
+              <Stack.Screen
+                name="EatsScreen"
+                options={{
+                  headerShown: false,
+                  gestureDirection: "horizontal",
+                  gestureEnabled: true,
+                }}
+                component={EatScreen}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </Provider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
